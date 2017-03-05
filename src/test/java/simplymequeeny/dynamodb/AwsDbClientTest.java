ed8f2a5e75fe61b021da19b51ca8fad830d2c6fc
simply_me_q <simply.me.queeny@gmail.com>
2017-03-05 00:38:29 -0500

    use mock for credential provider error

--- src/test/java/simplymequeeny/dynamodb/AwsDbClientTest.java
+++ src/test/java/simplymequeeny/dynamodb/AwsDbClientTest.java
@@ -1,19 +1,29 @@
 package simplymequeeny.dynamodb;
 
+import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
 import org.junit.Assert;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.Before;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
 import simplymequeeny.AbstractBaseTest;
 
 public class AwsDbClientTest extends AbstractBaseTest {
 
-    @Autowired
+    @Mock
     AwsDbClient dbClient;
 
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);    
+    }
+    
     @Test
     public void shouldConnectToDb() {
-        List<String> tables = dbClient.listTables().getTableNames();
+        when(dbClient.getTableNames()).thenReturn(Collections.EMPTY_LIST);
+        List<String> tables = dbClient.getTableNames();
         Assert.assertNotNull(tables);
     }   
 }
